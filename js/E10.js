caae9da3939370c8d59f418a97096b6cf51525a9
MarchoSaurio <112833842+MarchoSaurio@users.noreply.github.com>
2024-05-03 22:08:28 -0600

    Update E10.js

--- js/E10.js
+++ js/E10.js
@@ -1,29 +1,22 @@
-window.onlod = function(){
-
-    var info = document.getElementById("informacaion");
-
+window.onload = function() {
+    var info = document.getElementById("informacion");
+    
     var enlaces = document.getElementsByTagName("a");
+    info.innerHTML = "Numero de enlaces = " + enlaces.length;
 
-    var innerHTML = "Numero de enlaces = "+enlaces.length;
+    var mensaje = "El penultimo enlace apunta a: " + enlaces[enlaces.length - 2].href;
 
-    var mensaje = "El pen&uacute;iltmo enlace apunta a: "+enlaces[enlaces.length-2].href;
+    info.innerHTML = info.innerHTML + "<br/>" + mensaje;
 
-    info.innerHTML = info.innerHTML + "<br/>" +mensaje;
+    var contador=0;
+    for(var i=0; i<enlaces.length; i++)
+        if(enlaces[i].href == "http://google.com" || enlaces [i].href == "http://google.com/" )
+        contador++;
 
-    var contador = 0;
-
-    for (var i=0; i<enlaces.length; i++)
-
-    if(enlaces[i].href == "http://prueba" || enlaces[i].href == "http://prueba/")
-    contador++;
-
-    info.innerHTML = info.innerHTML + "<br/>" + contador + "enlaces apuntan a hhtp://prueba"
+    info.innerHTML = info.innerHTML + "<br/>" + contador + " enlaces apuntan a: http://google.com" ;
 
     var parrafos = document.getElementsByTagName("p");
-    enlaces = parrafos[2].getElementsByTagName("a");
+    enlaces = parrafos[2].getElementsByTagName("a"); 
 
-    info.innerHTML = info.innerHTML + "<br/>" + "Numero de enlaces de tercer parrafo = "+enlaces.length;
-
-
-
+    info.innerHTML = info.innerHTML + "<br/>" + "Numero de enlaces del tercer parrafo = " + enlaces.length;
 }
